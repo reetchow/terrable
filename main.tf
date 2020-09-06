92aeae26c9316da8ca34eb647665ff4251a3747c
Reet Chowdhary <reetchow@bu.edu>
2020-09-06 01:34:50 -0400

    working autoscale group with application load balancer

--- main.tf
+++ main.tf
@@ -2,20 +2,51 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0010d386b82bc06f0"
+# Data is a way to get information from the provider about existing resources
+data "aws_vpc"  "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0010d386b82bc06f0"
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, world!" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+    #!/bin/bash
+    echo "Hello, world!" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+  EOF
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "asg" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB" # Uses the health_check in the target group
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -23,9 +54,91 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-alb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.alb.id]
+  subnets = data.aws_subnet_ids.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "asg-target-group"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  # This is used to check if the service is up, also see health_check_type
+  # under the ASG as that makes sure this is used
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    port = var.server_port
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
